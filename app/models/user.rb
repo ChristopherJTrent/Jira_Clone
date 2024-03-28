424b5c711cc25283527c872e716600761d03c2c6
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-28 13:10:22 -0700

    create SER methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,23 +15,46 @@ class User < ApplicationRecord
 		end
 	end
 
-	def passwordValidator()
+	def reset_session_token!
+		self.session_token = generate_unique_session_token
+		save!
+		session_token
+	end
+
+	def ensure_session_token
+		self.session_token ||= generate_unique_session_token
+	end
+
+	private
+	def passwordValidator
+		return if password == nil
 		# this allows for a "correctHorseBatteryStaple" password to be valid
 		return if password.length > 16 && /.*[A-Z]/ =~ password
 		unless password.length > 8
-			errors.add :password, :too_short, message: "password must be more than 8 characters"
+			errors.add :password, :too_short, 
+					message: "password must be more than 8 characters"
 		end
 		unless /.*\d/ =~ password
-			errors.add :password, :reqiures_number, message: 'password must contain a number'
+			errors.add :password, :reqiures_number, 
+					message: 'password must contain a number'
 		end
 		unless /.*[a-z]/ =~ password
-			errors.add :password, :requires_lowercase, message: 'your password must include at least one lowercase letter'
+			errors.add :password, :requires_lowercase, 
+					message: 'password must include at least one lowercase letter'
 		end
 		unless /.*[A-Z]/ =~ password
-			errors.add :password, :requires_uppercase, message: 'your password must include at least one capital letter'
+			errors.add :password, :requires_uppercase, 
+					message: 'password must include at least one capital letter'
 		end
 		unless /.*[!@$#%^&*,.;:|]/ =~ password
-			errors.add :password, :requires_special_char, message: 'your password must include at least one special character (!@$#%^&*,.;:|)'
+			errors.add :password, :requires_special_char, 
+					message: 'password must include at least one special character (!@$#%^&*,.;:|)'
+		end
+	end
+	def generate_unique_session_token
+		loop do
+			token = SecureRandom.urlsafe_base64
+			return token unless User.exists?(session_token: token)
 		end
 	end
 end
