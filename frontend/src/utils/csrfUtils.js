68a0d2f2b07679db755062743dd5f98e7ee6011e
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-28 16:16:15 -0700

    implement utility functions

---
+++ frontend/src/utils/csrfUtils.js
@@ -0,0 +1,30 @@
+/**
+ * Restores the current session by requesting information from the server.
+ */
+export const restoreSession = async () => {
+	const res = await fetch('/api/session')
+	const token = res.headers.get('X-Csrf-Token')
+	sessionStorage.setItem('X-CSRF-Token', token)
+
+	sessionStorage.setItem('currentUser', 
+		JSON.stringify((await res.json()).user))
+}
+/**
+ * 
+ * @param {RequestInfo | URL} url 
+ * @param {RequestInit!} options
+ */
+export const csrfFetch = async (url, options) => {
+	options.method ??= 'GET'
+	options.headers ??= {}
+	//Method being case-insensitive isn't universally accepted.
+	options.method = options.method.toUpperCase()
+
+	if (options.method!== 'GET') {
+		options.headers['X-CSRF-Token'] = sessionStorage.getItem('X-CSRF-Token')
+		if (options.method === 'POST' && !(options.body instanceof FormData)) {
+			options.headers['Content-Type'] = 'application/json'
+		}
+	}
+	return await fetch(url, options)
+}
