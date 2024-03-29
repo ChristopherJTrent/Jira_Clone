763c86acc5e73d33a4bb18739231b1da97004baa
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-29 08:25:01 -0700

    implement restoring session on load

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { restoreSession } from './utils/csrfUtils.js'
 import router from './router.jsx'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router}>
-      
-		</RouterProvider>
-	</React.StrictMode>,
-)
+const initializeApp = () => {
+	ReactDOM.createRoot(document.getElementById('root')).render(
+		<React.StrictMode>
+			<RouterProvider router={router}>
+		
+			</RouterProvider>
+		</React.StrictMode>,
+	)
+}
+
+restoreSession().then(initializeApp)
