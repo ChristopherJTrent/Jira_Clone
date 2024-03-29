cffa5df83925c64ad259c56a417c57dd03b3cf66
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-29 16:59:45 -0700

    create main session page

--- frontend/src/pages/session.jsx
+++ frontend/src/pages/session.jsx
@@ -1,10 +1,25 @@
+import Background from '../assets/login-background.svg?react'
+import RegistrationLeft from '../assets/register-left.svg?react'
+import RegistrationRight from '../assets/register-right.svg?react'
 import SessionForm from '../components/SessionForm.jsx'
-import '../assets/login-background.svg'
+import { useState } from 'react'
+import './session.css'
 
 // eslint-disable-next-line react/prop-types
 export default function Session({type}) {
-	return (
+	const [flowStage, setFlowStage] = useState(0)
 
-		<SessionForm type={type}/>
+	return (<>
+		<div id='bg-container-1' className={'fullscreen' + (flowStage === 0 ? '' : ' transparent') }>
+			<Background />
+		</div>
+		<div id='bg-container-2' className={'leftpanel' + (flowStage === 0 ? '' : ' visible') }>
+			<RegistrationLeft />
+		</div>
+		<div id='bg-container-3' className={'rightpanel' + (flowStage === 0 ? '' : ' visible') }>
+			<RegistrationRight />
+		</div>
+		<SessionForm type={type} flowStage={flowStage} setFlowStage={setFlowStage}/>
+	</>
 	)
 }
