85c9618dc5fd42315d4c5c5c090dc11bc77adffe
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-31 10:46:29 -0700

    create password validator display

---
+++ frontend/src/components/passwordValidatorDisplay.jsx
@@ -0,0 +1,54 @@
+import './passwordValidatorDisplay.css'
+// eslint-disable-next-line react/prop-types
+export default function PasswordValidatorDisplay({score}) {
+	const getClassForScore = () => {
+		if (score >= 6) return 'darkGreen'
+		switch(score){
+		case 5:
+			return 'green'
+		case 4:
+			return 'yellow'
+		case 3:
+			return 'orange'
+		case 2:
+			return 'red'
+		case 1:
+		default:
+			return 'gray'
+		}
+	}
+
+	const getDisplayText = () => {
+		if (score >= 6) return 'Very strong'
+		switch(score) {
+		case 5: 
+			return 'Strong'
+		case 4: 
+			return 'Good'
+		case 3:
+			return 'Fair'
+		case 2:
+			return 'Weak'
+		case 1:
+			return 'Password must have at least 8 characters'
+		default:
+			return ''
+		}
+
+	}
+
+	return (
+		<>
+			<div id='passwordValidationDisplayContainer'>
+				<div className={getClassForScore()}>
+					<span className="ticks"></span>
+				</div>
+			</div>
+			<div role='status'>
+				<p>
+					{getDisplayText()}
+				</p>
+			</div>
+		</>
+	)
+}
