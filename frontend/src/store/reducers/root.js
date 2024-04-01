8458022d20fd916b45d9457c73cf651c33fbcf31
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-01 15:47:25 -0700

    scaffold store reducer

--- frontend/src/store/reducers/root.js
+++ frontend/src/store/reducers/root.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
+import projectsReducer from './projects.js'
 import sessionReducer from './session.js'
 import userReducer from './user.js'
 const rootReducer = combineReducers({
 	users: userReducer,
-	session: sessionReducer
+	session: sessionReducer,
+	projects: projectsReducer
 })
 
 export default rootReducer
