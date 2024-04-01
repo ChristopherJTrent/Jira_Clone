54f43b1d75f1fe0ed0f3bd5a5d9ce16a654ed995
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-01 09:38:48 -0700

    add minimum necessary splash page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-
+import JiraHeader from '../assets/jira-header.svg?react'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 export default function Navbar() {
 
 	return <nav>
 		<ul>
-
-			
+			<li>
+				<JiraHeader />
+			</li>
+			<li className="floatRight">
+				<NavLink to='/login'>Sign In</NavLink>
+			</li>
 		</ul>
 	</nav>
 }
--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import './layout.css'
+import Navbar from './components/Navbar.jsx'
 
 function Layout() {
 	return (
 		<>
+			<Navbar />
 			<Outlet />
 		</>
 	)
