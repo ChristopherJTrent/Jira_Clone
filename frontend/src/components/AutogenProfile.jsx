d37afa75fe4f182dce3c7d494e5834d75842d86b
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-02 09:51:15 -0700

    add user icon to navbar

--- frontend/src/components/AutogenProfile.jsx
+++ frontend/src/components/AutogenProfile.jsx
@@ -1,14 +1,17 @@
 import './AutogenProfile.css'
 
 // eslint-disable-next-line react/prop-types
-export default function AutogenProfile({name}) {
+export default function AutogenProfile({name, onClick}) {
 	// eslint-disable-next-line react/prop-types
 	const initials = name.split(' ')
 		.map((v) => v[0])
 		.slice(0, 2)
 		.join('')
 	
-	return <span className='autogen-profile'>
-		{initials}
+	return <span className='autogen-profile'
+		onClick={onClick}>
+		<p>
+			{initials}
+		</p>
 	</span>
 }
