27cc45042fc286e14f2d0b51a3d7ea4753214442
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-01 21:15:30 -0700

    start working on main project list

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,57 @@
-import { currentUserSelector } from '../store/selectors/session.js'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProjects } from '../store/reducers/projects.js'
+import HeaderStar from '../assets/header-star.svg?react'
+import { projectSelector } from '../store/selectors/project.js'
+import Star from '../assets/star.svg?react'
+import { useEffect } from 'react'
 
 export default function Home() {
-	const user = useSelector(currentUserSelector)
-	return <h1>Hello, {user.username}!</h1>
+	const dispatch = useDispatch()
+	const posts = useSelector(projectSelector)
+	const users = useSelector(state => state.users)
+
+	useEffect(() => {
+		dispatch(fetchProjects())
+	}, [])
+
+	console.log(posts)
+	console.log(users)
+
+	return <table>
+		<thead>
+			<tr>
+				<td><HeaderStar /></td>
+				<th>Name</th>
+				<th>Key</th>
+				<th>Type</th>
+				<th>Lead</th>
+				<th>More Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			{
+				posts.map((post, i) => {
+					if (!users[post.ownerId]) {
+						dispatch(fetchUser())
+					}
+					return <tr key={i}>
+						<td>
+							<Star />
+						</td>
+						<td>{post.title}</td>
+						<td>{post.key}</td>
+						<td>Team-managed software</td>
+						<td>{users[post.ownerId]?.username}</td>
+					</tr>
+				})
+			}
+		</tbody>
+	</table>
+	
+	// <ul>
+	// 	{posts.map((v, i) => <li key={i}>
+	// 		<h1>{v.title}</h1>
+	// 		<p>{v.id}</p>
+	// 	</li>)}
+	// </ul>
 }
