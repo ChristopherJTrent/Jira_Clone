7a9b17c5552f9913310e55078be27c9b7d8381b6
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-03 09:29:38 -0700

    create modal layout

--- frontend/src/components/DeleteProjectConfirmationModal.jsx
+++ frontend/src/components/DeleteProjectConfirmationModal.jsx
@@ -1,5 +1,17 @@
 
-export default function DeleteProjectConfirmationModal({enabled, setEnabled}) {
-	
-	return <></>
+export default function DeleteProjectConfirmationModal({enabled, setEnabled, post}) {
+
+	return <>
+		<h1>Delete Project?</h1>
+		<p>This project, along with its issues, components, attachments, and versions will be permanently deleted immediately.</p>
+		<p className="red">Deleted projects CANNOT be restored.</p>
+		<div id='bottomButtonContainer'>
+			<button>
+				Cancel
+			</button>
+			<button>
+				Delete
+			</button>
+		</div>
+	</>
 }
