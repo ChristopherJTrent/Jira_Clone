9a9013ec37603179b404c56dc83132c4e2f0c2b2
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-10 08:20:22 -0700

    add redirect to root page for logged-out users on protected routes

--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -1,8 +1,19 @@
+import { Outlet, useNavigate } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import {useSelector} from 'react-redux'
 import './layout.css'
 
 function Layout() {
+
+	const currentUser = useSelector(state => state.session.currentUserId)
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (! currentUser) {
+			navigate('/')
+		}
+	}, [currentUser])
 
 	return (
 		<>
