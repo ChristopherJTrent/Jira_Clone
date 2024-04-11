261a6d5a870f21246ca217cfee3aadace230094a
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-11 11:07:24 -0700

    add prerequisite selectors

--- frontend/src/store/selectors/epics.js
+++ frontend/src/store/selectors/epics.js
@@ -1,4 +1,5 @@
 import {createSelector} from 'reselect'
+import { projectSelector } from './project.js'
 
 export const selectEpicById = epicId => state => state.epics[epicId]
 export const selectAllEpics = () => state => state.epics
@@ -8,4 +9,8 @@ export const selectEpicsForProject = projectId => createSelector(
 		const values = Object.values(epics)?.filter((v) => v.projectId === projectId)
 		return values.length > 0 ? values : undefined
 	}
+)
+export const selectParentOfEpic = epicId => createSelector(
+	[projectSelector, selectEpicById(epicId)],
+	(projects, epic) => projects.find(project => project.id === epic.projectId)
 )
--- frontend/src/store/selectors/project.js
+++ frontend/src/store/selectors/project.js
@@ -4,3 +4,4 @@ export const projectSelector = createSelector(
 	state => state.projects,
 	projects => Object.values(projects)
 )
+export const getProjectById = projectId => state => state.projects[projectId]
