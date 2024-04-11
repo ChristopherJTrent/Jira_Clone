0eef0b84e0d0633a560c9dd527b5bfe9f5d9c9cd
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-04-11 11:24:02 -0700

    add requested QOL to editing epic names

--- frontend/src/components/Epics/EpicHeader.jsx
+++ frontend/src/components/Epics/EpicHeader.jsx
@@ -1,8 +1,10 @@
 import { deleteEpic, updateEpic } from '../../store/reducers/epics.js'
 import Collapse from '../../assets/collapse-image.svg?react'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { useState } from 'react'
 import './EpicHeader.css'
+import { selectParentOfEpic } from '../../store/selectors/epics.js'
+import useFocus from '../../utils/useFocus.js'
 
 
 export default function EpicHeader({epic, collapsed, setCollapsed, tasks}) {
@@ -10,6 +12,9 @@ export default function EpicHeader({epic, collapsed, setCollapsed, tasks}) {
 	const [epicName, setEpicName] = useState(epic.title)
 	const [isEditingName, setIsEditingName] = useState(false)
 	const [modalState, setModalState] = useState({active: false})
+	const currentUserId = useSelector(state => state.session.currentUserId)
+	const parentProject = useSelector(selectParentOfEpic(epic.id))
+	const [focusRef, setFocus] = useFocus()
 
 	const handleSubmit = e => {
 		e.preventDefault()
@@ -21,6 +26,7 @@ export default function EpicHeader({epic, collapsed, setCollapsed, tasks}) {
 
 	const handleDelete = e => {
 		e.preventDefault()
+		setModalState({...modalState, active:false})
 		dispatch(deleteEpic(epic.id))
 	}
 
@@ -51,12 +57,22 @@ export default function EpicHeader({epic, collapsed, setCollapsed, tasks}) {
 				e.preventDefault()
 				setEpicName(e.target.value)
 			}}
-			disabled={!isEditingName}/>
+			disabled={!isEditingName}
+			ref={focusRef}
+			onKeyDown={(e) => {
+				if (e.key === 'Enter') {
+					handleSubmit(e)
+				}
+			}}/>
 		{isEditingName && <button 
 			onClick={handleSubmit}
 			className='saveButton'>Save</button>
 			|| <button 
-				onClick={() => setIsEditingName(true)}
+				onClick={() => {
+					setIsEditingName(true)
+					//this doesn't work if you do it immediately.
+					setTimeout(() => setFocus(), 1)
+				}}
 				className='editButton'>🖉</button>}
 		<span className='issueDisplay'>
 			({`${tasks.length === 0 ? 
@@ -64,11 +80,12 @@ export default function EpicHeader({epic, collapsed, setCollapsed, tasks}) {
 				tasks.length} issue${tasks.length === 1 ? '' : 's'}`})
 		</span>
 		<span className='todoMarker'>TO DO</span>
-		<span className='floatRight'>
+		{currentUserId === parentProject.ownerId 
+		&& <span className='floatRight'>
 			<button className='styled danger noBackground'
 				onClick={() => setModalState({...modalState, active: true})}>
 				Delete Epic
 			</button>
-		</span>
+		</span>}
 	</div>
 }
